f0cf9809d888d22417ec12cfd2eac50e749a2c75
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-02-07 15:35:45 -0300

    Delete 16_AuthenticationWithJWT directory

--- 16_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.erudio.repositories;
-
-import br.com.erudio.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository public interface BookRepository extends JpaRepository<Book, Long> {}
