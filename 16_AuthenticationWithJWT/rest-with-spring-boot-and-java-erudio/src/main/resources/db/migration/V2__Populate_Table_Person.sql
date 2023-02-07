01fcd167882e2f0d993e33fdcdf6b819202969d5
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-02-07 09:50:38 -0300

    Delete 16_AuthenticationWithJWT directory

--- 16_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1,'Salvador','Vini','Male','Jesus'),
-    (3,'Salvador','Mila','Female','Carmo'),
-    (6,'Sp','Neymar','Male','Junior'),
-    (7,'Salvador','Adriano','Male','Imperador');
